4c194c182f83b8ac66d232c3882eeba561c29bb4
Bongumusa Ryan Mkungo <bongumusamkhungo04@gmail.com>
2023-05-18 13:21:40 +0200

    Add project files.

---
+++ SportsStore/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SportsStore.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public byte[] AvatarImage { get; set; } = new byte[0];
+    }
+}
